6b152504bf986aef703efb583af2ac1ea69fbcdb
tolgazorlu <tolgazorlu17@gmail.com>
2026-05-04 14:21:25 +0300

    fix: detect formula path dynamically in brew fix step

--- Formula/btrack.rb
+++ Formula/btrack.rb
@@ -6,30 +6,26 @@ class Btrack < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tolgazorlu/btrack/releases/download/v#{version}/btrack-darwin-arm64"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_ARM64"
+      url "https://github.com/tolgazorlu/btrack/releases/download/v0.1.0/btrack-darwin-arm64.tar.gz"
+      sha256 "ae059c7d8796856806f075a61b6872bb22e2012dfca55d4278363655d79b92e4"
     else
-      url "https://github.com/tolgazorlu/btrack/releases/download/v#{version}/btrack-darwin-amd64"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_AMD64"
+      url "https://github.com/tolgazorlu/btrack/releases/download/v0.1.0/btrack-darwin-amd64.tar.gz"
+      sha256 "557843b549a0ff8bd6b0683006f2d04ea6310e25c424556b5b9b970538c8c264"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/tolgazorlu/btrack/releases/download/v#{version}/btrack-linux-arm64"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX_ARM64"
+      url "https://github.com/tolgazorlu/btrack/releases/download/v0.1.0/btrack-linux-arm64.tar.gz"
+      sha256 "9959d08f31d3a13e6a4e08117be73f94c48b00d872fc75b57682aed50a217e05"
     else
-      url "https://github.com/tolgazorlu/btrack/releases/download/v#{version}/btrack-linux-amd64"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256_LINUX_AMD64"
+      url "https://github.com/tolgazorlu/btrack/releases/download/v0.1.0/btrack-linux-amd64.tar.gz"
+      sha256 "d203a0df99a20b213d60ef42321be8a6acb59c1fcc9ff68461ad40f9e0df3afa"
     end
   end
 
   def install
-    bin.install Dir["btrack*"].first => "btrack"
-  end
-
-  def post_install
-    (var/"btrack").mkpath
+    bin.install "btrack"
   end
 
   test do
