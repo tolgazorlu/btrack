717eb185e5ea08a5b1d803bb39de3a9e06d19f73
tolgazorlu <tolgazorlu17@gmail.com>
2026-09-16 19:43:58 +0300

    refactor: remove Google Calendar sync

--- cmd/stop.go
+++ cmd/stop.go
@@ -11,7 +11,6 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/tolgazorlu/btrack/internal/config"
 	"github.com/tolgazorlu/btrack/internal/daemon"
-	"github.com/tolgazorlu/btrack/internal/gcal"
 	"github.com/tolgazorlu/btrack/internal/ui"
 )
 
@@ -113,16 +112,6 @@ Tips:
 			ui.KV("message", ui.StyleHighlight.Render(message))
 		}
 
-		if cfg, err := config.Load(); err == nil && cfg.GCal.AutoSync && cfg.GCal.ClientID != "" {
-			fmt.Printf("%s%s  syncing to Google Calendar…\r", ui.Indent, ui.StyleDimmed.Render(ui.Sym.Up))
-			svc, err := gcal.NewService(cfg.GCal.ClientID, cfg.GCal.ClientSecret, config.DataDir())
-			if err == nil {
-				_, err := gcal.PushSession(svc, cfg.GCal.CalendarID, sess.TaskName, sess.Project, start, end)
-				if err == nil {
-					ui.Sign(ui.StyleSuccess.Render(ui.Sym.Up), ui.StyleDimmed.Render("synced to Google Calendar"))
-				}
-			}
-		}
 		ui.Blank()
 		return nil
 	},
